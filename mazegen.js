daafc5d9a436a112526bc770ba2a8bf6b916ebe4
kylewong975 <kyle.wong975@gmail.com>
2020-06-03 16:19:38 -0700

    Fix maze dead ends

--- mazegen.js
+++ mazegen.js
@@ -39,21 +39,21 @@ function generate_maze(x,y) {
 
 function display_maze(m) {
     var text= [];
-    for (var j= 0; j<m.x+1; j++) {
+    for (var j= 0; j<2*m.x+1; j++) {
         var line= [];
         if (0 == j%2)
-            for (var k=0; k<m.y+1; k++)
+            for (var k=0; k<2*m.y+1; k++)
                 if (0 == k%2) 
                     line[k]= '+';
                 else
-                    if (j>0 && m.verti[j][k])
+                    if (j>0 && m.verti[j/2-1][Math.floor(k/2)])
                         line[k]= ' ';
                     else
                         line[k]= '-';
         else
-            for (var k=0; k<m.y+1; k++)
+            for (var k=0; k<2*m.y+1; k++)
                 if (0 == k%2)
-                    if (k>0 && m.horiz[j-1][k])
+                    if (k>0 && m.horiz[(j-1)/2][k/2-1])
                         line[k]= ' ';
                     else
                         line[k]= '|';
@@ -62,37 +62,8 @@ function display_maze(m) {
         // if (0 == j) line[1]= ' ';
         // if (m.x-1 == j) line[m.y]= ' ';
         if(j == 1) line[1] = 'S';
-        if(j == 9) line[9] = 'E';
+        if(j == 2*m.x-1) line[2*m.y-1] = 'E';
         text.push(line.join(''));
     }
     return text;
 }
-
-// display(m) {
-//     var text= [];
-//     for (var j= 0; j<m.x*2+1; j++) {
-//         var line= [];
-//         if (0 == j%2)
-//             for (var k=0; k<m.y*4+1; k++)
-//                 if (0 == k%4) 
-//                     line[k]= '+';
-//                 else
-//                     if (j>0 && m.verti[j/2-1][Math.floor(k/4)])
-//                         line[k]= ' ';
-//                     else
-//                         line[k]= '-';
-//         else
-//             for (var k=0; k<m.y*4+1; k++)
-//                 if (0 == k%4)
-//                     if (k>0 && m.horiz[(j-1)/2][k/4-1])
-//                         line[k]= ' ';
-//                     else
-//                         line[k]= '|';
-//                 else
-//                     line[k]= ' ';
-//         if (0 == j) line[1]= line[2]= line[3]= ' ';
-//         if (m.x*2-1 == j) line[4*m.y]= ' ';
-//         text.push(line.join('')+'\r\n');
-//     }
-//     return text.join('');
-// }
